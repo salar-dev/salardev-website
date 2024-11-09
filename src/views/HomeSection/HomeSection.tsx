1b49352d33608300fd6182b698b44772f18044a9
salar-dev <developer.salar@gmail.com>
2024-11-09 16:51:45 +0300

    04511109

--- src/views/HomeSection/HomeSection.tsx
+++ src/views/HomeSection/HomeSection.tsx
@@ -9,6 +9,9 @@ import 'react-tooltip/dist/react-tooltip.css'
 import { Tooltip } from 'react-tooltip'
 
 function HomeSection() {
+    const openInNewTab = (url: string): void => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
     const { t } = useTranslation();
     return (
         <div className='homeSection'>
@@ -18,19 +21,19 @@ function HomeSection() {
                     <h1><span>&lt;</span>salar<span className='salardev_space'> </span>dev<span>/&gt;</span></h1>
                     <p className='salardev_about'>{t('salardev_about')}</p>
                     <div className="social" >
-                        <div className="social_contaner" id='insta' data-tooltip-id="my-tooltip" data-tooltip-content="Instagram => salar_dev" style={{ background: "#4075cc47" }}>
+                        <div className="social_contaner" id='insta' data-tooltip-id="my-tooltip" data-tooltip-content="Instagram => salar_dev" style={{ background: "#4075cc47" }} onClick={() => openInNewTab('https://www.instagram.com/salar_dev')}>
                             <InstagramIcon className='social_contaner_icon' />
                         </div>
 
-                        <div className="social_contaner" id='youtube' data-tooltip-id="my-tooltip" data-tooltip-content="YouTube => Salar Dev" style={{ background: "#e9191959" }}>
+                        <div className="social_contaner" id='youtube' data-tooltip-id="my-tooltip" data-tooltip-content="YouTube => Salar Dev" style={{ background: "#e9191959" }} onClick={() => openInNewTab('https://www.youtube.com/@SalarDev')}>
                             <YouTubeIcon className='social_contaner_icon' />
                         </div>
 
-                        <div className="social_contaner" id='whatsApp' data-tooltip-id="my-tooltip" data-tooltip-content="WhatsApp => +9647508684288" style={{ background: "#30ce1862" }}>
+                        <div className="social_contaner" id='whatsApp' data-tooltip-id="my-tooltip" data-tooltip-content="WhatsApp => +9647508684288" style={{ background: "#30ce1862" }} onClick={() => openInNewTab('https://wa.me/+9647508684288')}>
                             <WhatsAppIcon className='social_contaner_icon' />
                         </div>
 
-                        <div className="social_contaner" id='github' data-tooltip-id="my-tooltip" data-tooltip-content="GitHub => salar-dev" style={{ background: "#757a747d" }}>
+                        <div className="social_contaner" id='github' data-tooltip-id="my-tooltip" data-tooltip-content="GitHub => salar-dev" style={{ background: "#757a747d" }} onClick={() => openInNewTab('https://github.com/salar-dev')}>
                             <GitHubIcon className='social_contaner_icon' />
                         </div>
 
